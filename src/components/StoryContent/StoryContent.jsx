cca94bbaeda828d57f34526f5e057541bd24af53
Vadim Kostrov <85374361+Tiyoriyo@users.noreply.github.com>
2024-02-12 00:20:40 +0400

    update

--- src/components/StoryContent/StoryContent.jsx
+++ src/components/StoryContent/StoryContent.jsx
@@ -12,6 +12,7 @@ function StoryContent({
           <img src="src/assets/cats/catn3.png" className="w-[150px] border-b-4 border-[#C37272] border-dashed pb-4" alt="" />
           <div className="flex flex-col gap-8 xl:max-w-[550px] items-center font-semibold xl:font-light text-[18px] capitalize pb-4 text-justify">
             <p className="text-[#2D2D2D]">In the heart of our shared love for all things feline, Fairy Tails was born—a tale spun by two kindred spirits with a passion for whiskers, purrs, and the enchanting world of cats. Our journey began when [Your Names], the dreamweavers behind Fairy Tails, realized the need for a haven where cats could be surrounded by love and care, even when their human companions were away.</p>
+            <p className="text-[#C37272] drop-shadow-lg">- - &#x25CF; - -</p>
             <p className="text-[#2D2D2D]">As lifelong cat enthusiasts, our story is one of genuine connection and understanding. We found inspiration in the unique personalities and quirks of our own feline companions, recognizing that each cat is a magical being with distinct needs and preferences. It was this realization that ignited the spark, propelling us into the creation of Fairy Tails—a sanctuary where cats are not merely visitors but cherished members of our ever-growing story.</p>
             <p className="font-meow text-[24px] text-[#C37272] font-light border-b-4 border-[#C37272]/70 border-dashed xl:border-none pb-4">~ Meghan & Paulina ~</p>
           </div>
