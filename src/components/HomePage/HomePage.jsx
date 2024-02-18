130cd703af785e71c9db725e10d1cf55b72e718d
Vadim Kostrov <85374361+Tiyoriyo@users.noreply.github.com>
2024-02-18 11:56:22 +0400

    Update

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -18,7 +18,7 @@ function HomePage() {
           />
           <div>
             <img
-              src="src/assets/cats/catn1.png"
+              src="./src/assets/cats/catn1.png"
               className="absolute
             w-[65px] xs:w-[70px] sm:w-[80px] md:w-[90px] lg:w-[100px] xl:w-[110px]
             top-[20px] sm:top-[40px] md:top-[50px] lg:top-[65px] xl:top-[80px]
@@ -26,7 +26,7 @@ function HomePage() {
               alt=""
             />
             <img
-              src="src/assets/cats/catn2.png"
+              src="./src/assets/cats/catn2.png"
               className="absolute
             w-[65px] xs:w-[70px] sm:w-[80px] md:w-[90px] lg:w-[100px] xl:w-[110px]
             top-[115px] xs:top-[130px] sm:top-[165px] md:top-[215px] lg:top-[250px] xl:top-[290px]
@@ -34,7 +34,7 @@ function HomePage() {
               alt=""
             />
             <img
-              src="src/assets/cats/catn3.png"
+              src="./src/assets/cats/catn3.png"
               className="absolute
             w-[100px] xs:w-[110px] sm:w-[125px] md:w-[150px] lg:w-[175px] xl:w-[190px]
             top-[75px] xs:top-[90px] sm:top-[115px] md:top-[140px] lg:top-[165px] xl:top-[190px]
@@ -42,7 +42,7 @@ function HomePage() {
               alt=""
             />
             <img
-              src="src/assets/cats/catn4.png"
+              src="./src/assets/cats/catn4.png"
               className="
             absolute
             w-[40px] xs:w-[45px] sm:w-[55px] md:w-[65px] lg:w-[72px] xl:w-[80px]
